b821661643008e82ef70571b016cd046cc682898
adeebashraf <adeeb.ashraf@gmail.com>
2014-07-29 09:07:10 -0700

    Changes for Facebook OAuth
    Facebook model added.  Changes made in Application controller and in
    index.scala.html view.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,22 @@
 package controllers;
 
 import play.*;
+import play.data.Form;
 import play.mvc.*;
-
+import models.Facebook;
 import views.html.*;
 
 public class Application extends Controller {
+	
+	static Form<Facebook> fbForm = Form.form(Facebook.class);
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        //return ok(index.render("Your new application is ready."));
+    	return ok(views.html.index.render(fbForm));
+    }
+    
+    public static Result facebook() {
+    	return TODO;
     }
 
 }
---
+++ app/models/Facebook.java
@@ -0,0 +1,9 @@
+package models;
+
+public class Facebook {
+
+	public Facebook() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
